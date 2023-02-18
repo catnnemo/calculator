003b02fc9e1e7760b291d484a12f8b0d1ed22b3a
catnnemo <njpianoforte@gmail.com>
2023-02-18 14:31:20 -0500

    First code

---
+++ calculator.js
@@ -0,0 +1,37 @@
+const regions = {
+    eastern_nc: {
+      condition_pave: 2.25,
+      resurface_overlay: 2.1,
+      new_construction: 4.15,
+      saw_cut_patch_rip_out_and_replace: 4.45,
+      skim_patch: 2.5,
+      seal_coat: 0.37,
+      striping: 0.7
+    },
+    the_triangle: {
+      condition_pave: 2.25,
+      resurface_overlay: 2.05,
+      new_construction: 4.15,
+      saw_cut_patch_rip_out_and_replace: 4.6,
+      skim_patch: 2,
+      seal_coat: 0.37,
+      striping: 1.8
+    },
+    wilmington: {
+      condition_pave: 2.5,
+      resurface_overlay: 2.15,
+      new_construction: 4.15,
+      saw_cut_patch_rip_out_and_replace: 4.15,
+      skim_patch: 2.05,
+      seal_coat: 0.3,
+      striping: 2
+    },
+    fayetteville: {
+      condition_pave: 2.48,
+      resurface_overlay: 2.07,
+      new_construction: 3.6,
+      saw_cut_patch_rip_out_and_replace: 4.05,
+      skim_patch: 2.25,
+      seal_coat: 0.35,
+      striping: 2
+    } } 

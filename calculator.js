70559eab7602734dbd7ed5af4261b602444080d3
catnnemo <120193669+catnnemo@users.noreply.github.com>
2023-02-19 17:37:50 -0500

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,37 +1,394 @@
-const regions = {
+
+const data = {
     eastern_nc: {
-      condition_pave: 2.25,
-      resurface_overlay: 2.1,
-      new_construction: 4.15,
-      saw_cut_patch_rip_out_and_replace: 4.45,
-      skim_patch: 2.5,
-      seal_coat: 0.37,
-      striping: 0.7
+      pave_price_minimum: 2500,
+      seal_price_minimum: 875,
+      condition_pave: {
+        u2: 2.25,
+        u4: 2.20,
+        u6: 2.15,
+        u8: 2.10,
+        u15: 2.05,
+        s: 2.00,
+        min: 475,
+        0: 0
+      },
+      resurface_overlay:{
+        u2: 2.10,
+        u4: 2.05,
+        u6: 2.00,
+        u8: 2.00,
+        u15: 2.00,
+        s: 2.00,
+        min: 525,
+        0: 0
+      },
+      new_construction:{
+        u2: 4.15,
+        u4: 4.10,
+        u6: 4.05,
+        u8: 4.00,
+        u15: 3.95,
+        s: 3.90,
+        min: 625,
+        0: 0
+      },
+      saw_cut_patch:{
+        u2: 4.45,
+        u4: 4.40,
+        u6: 4.35,
+        u8: 4.30,
+        u15: 4.25,
+        s: 4.00,
+        min: 300,
+        0: 0
+      },
+      skim_patch:{
+        u2: 2.50,
+        u4: 2.25,
+        u6: 2.10,
+        u8: 2.02,
+        u15: 2.00,
+        s: 1.95,
+        min: 1500,
+        0: 0
+      },
+      seal_coat:{
+        u2: 0.37,
+        u4: 0.36,
+        u6: 0.34,
+        u8: 0.32,
+        u15: 0.27,
+        s: 0.25,
+        min: 4000,
+        0: 0
+      }
     },
     the_triangle: {
-      condition_pave: 2.25,
-      resurface_overlay: 2.05,
-      new_construction: 4.15,
-      saw_cut_patch_rip_out_and_replace: 4.6,
-      skim_patch: 2,
-      seal_coat: 0.37,
-      striping: 1.8
+      pave_price_minimum: 3000,
+      seal_price_minimum: 1050,
+      condition_pave: {
+        u2: 2.25,
+        u4: 2.20,
+        u6: 2.15,
+        u8: 2.10,
+        u15: 2.05,
+        s: 2.00,
+        min: 475,
+        0: 0
+    },
+      resurface_overlay:{
+        u2: 2.05,
+        u4: 2.00,
+        u6: 1.95,
+        u8: 1.90,
+        u15: 1.85,
+        s: 1.80,
+        min: 525,
+        0: 0
+      },
+      new_construction:{
+        u2: 4.15,
+        u4: 4.10,
+        u6: 4.05,
+        u8: 4.00,
+        u15: 3.95,
+        s: 3.90,
+        min: 625,
+        0: 0
+      },
+      saw_cut_patch:{
+        u2: 4.60,
+        u4: 4.55,
+        u6: 4.50,
+        u8: 4.45,
+        u15: 4.40,
+        s: 4.35,
+        min: 300,
+        0: 0
+      },
+      skim_patch:{
+        u2: 2.00,
+        u4: 1.95,
+        u6: 1.90,
+        u8: 1.85,
+        u15: 1.80,
+        s: 1.75,
+        min: 1500,
+        0: 0
+      },
+      seal_coat:{
+        u2: 0.37,
+        u4: 0.36,
+        u6: 0.35,
+        u8: 0.32,
+        u15: 0.27,
+        s: 0.25,
+        min: 4000,
+        0: 0
+      }
     },
     wilmington: {
-      condition_pave: 2.5,
-      resurface_overlay: 2.15,
-      new_construction: 4.15,
-      saw_cut_patch_rip_out_and_replace: 4.15,
-      skim_patch: 2.05,
-      seal_coat: 0.3,
-      striping: 2
+      pave_price_minimum: 3500,
+      seal_price_minimum: 1225,
+      condition_pave: {
+        u2: 2.50,
+        u4: 2.45,
+        u6: 2.40,
+        u8: 2.35,
+        u15: 2.30,
+        s: 2.25,
+        min: 475,
+        0: 0
     },
-    fayetteville: {
-      condition_pave: 2.48,
-      resurface_overlay: 2.07,
-      new_construction: 3.6,
-      saw_cut_patch_rip_out_and_replace: 4.05,
-      skim_patch: 2.25,
-      seal_coat: 0.35,
-      striping: 2
-    } } 
+      resurface_overlay:{
+        u2: 2.15,
+        u4: 2.15,
+        u6: 2.05,
+        u8: 2.00,
+        u15: 2.00,
+        s: 1.95,
+        min: 525,
+        0: 0
+      },
+      new_construction:{
+        u2: 4.15,
+        u4: 4.10,
+        u6: 4.05,
+        u8: 4.00,
+        u15: 3.95,
+        s: 3.90,
+        min: 625,
+        0: 0
+      },
+      saw_cut_patch:{
+        u2: 4.15,
+        u4: 4.10,
+        u6: 4.05,
+        u8: 4.00,
+        u15: 3.95,
+        s: 3.90,
+        min: 300,
+        0: 0
+      },
+      skim_patch:{
+        u2: 2.05,
+        u4: 2.00,
+        u6: 1.95,
+        u8: 1.90,
+        u15: 1.85,
+        s: 1.80,
+        min: 1500,
+        0: 0
+      },
+      seal_coat:{
+        u2: 0.30,
+        u4: 0.29,
+        u6: 0.27,
+        u8: 0.25,
+        u15: 0.23,
+        s: 0.21,
+        min: 4000,
+        0: 0
+      }
+    },
+    fayetville: {
+      pave_price_minimum: 35000,
+      seal_price_minimum: 3750,
+      condition_pave: {
+        u2: 2.48,
+        u4: 2.43,
+        u6: 2.38,
+        u8: 2.33,
+        u15: 2.28,
+        s: 2.23,
+        min: 475,
+        0: 0
+      },
+      resurface_overlay:{
+        u2: 2.07,
+        u4: 2.02,
+        u6: 1.97,
+        u8: 1.92,
+        u15: 1.87,
+        s: 1.82,
+        min: 525,
+        0: 0
+      },
+      new_construction:{
+        u2: 3.60,
+        u4: 3.55,
+        u6: 3.50,
+        u8: 3.45,
+        u15: 3.40,
+        s: 3.35,
+        min: 625,
+        0: 0
+      },
+      saw_cut_patch:{
+        u2: 4.05,
+        u4: 4.00,
+        u6: 3.95,
+        u8: 3.90,
+        u15: 3.85,
+        s: 3.80,
+        min: 300,
+        0: 0
+      },
+      skim_patch:{
+        u2: 2.05,
+        u4: 2.00,
+        u6: 1.95,
+        u8: 1.90,
+        u15: 1.85,
+        s: 1.80,
+        min: 1500,
+        0: 0
+      },
+      seal_coat:{
+        u2: 0.35,
+        u4: 0.34,
+        u6: 0.32,
+        u8: 0.30,
+        u15: 0.25,
+        s: 0.21,
+        min: 4000,
+        0: 0
+      }
+    }
+} 
+
+function area_code(area) {
+  if (area <= 0){
+      return 0;
+    } else if (area<=2000) {
+      return "u2";
+    } else if (area<=4000) {
+      return "u4";
+    } else if (area<=6000){
+      return "u6";
+    } else if (area<=8000){
+      return "u8";
+    } else if (area <= 15000){
+      return "u15";
+    } else if (area > 15000){
+      return "s";
+    } else if (area.value.length == 0){
+      return 0;
+    } else {
+      return 0;
+    }
+}
+
+//TO DO: add in minimum price adjustments
+function adjustForPaveMinimumPrice(region, type, area, price){
+  var price_min = data[region].pave_price_minimum;
+  if (area < data[region][type].min){
+    return price_min;
+  } else if (price < price_min){
+    return price_min;
+  } else {
+    return price;
+  }
+}
+
+
+// calculate function can't handle numbers with commas
+// add data validation and cleaning
+
+function calculate(){
+  var region = document.getElementById("region").value;
+
+  var condition_pave_area = parseFloat(document.getElementById("condition_pave").value);
+  var resurface_overlay_area = parseFloat(document.getElementById("resurface_overlay").value);
+  var new_construction_area = parseFloat(document.getElementById("new_construction").value);
+  var saw_cut_patch_area = parseFloat(document.getElementById("saw_cut_patch").value);
+  var skim_patch_area = parseFloat(document.getElementById("skim_patch").value);
+  var seal_coat_area = parseFloat(document.getElementById("seal_coat").value);
+  
+
+  var condition_pave_cost = condition_pave_area * data[region].condition_pave[area_code(condition_pave_area)];
+  var resurface_overlay_cost = resurface_overlay_area * data[region].resurface_overlay[area_code(resurface_overlay_area)];
+  var new_construction_cost = new_construction_area * data[region].new_construction[area_code(new_construction_area)];
+  var saw_cut_patch_cost = saw_cut_patch_area * data[region].saw_cut_patch[area_code(saw_cut_patch_area)];
+  var skim_patch_cost = skim_patch_area * data[region].skim_patch[area_code(skim_patch_area)];
+  var seal_coat_cost = seal_coat_area * data[region].seal_coat[area_code(seal_coat_area)];
+
+  var stripe_cost = 50 * parseFloat(document.getElementById("striping").value);
+
+  //condition_pave_cost = adjustForPaveMinimumPrice(region, "condition_pave", condition_pave_area, condition_pave_cost);
+
+  //display result
+  document.getElementById("total_area").innerHTML = "Total Area: " + parseFloat(condition_pave_area + resurface_overlay_area + new_construction_area + saw_cut_patch_area + skim_patch_area + seal_coat_area);
+
+  //document.getElementById("test").innerHTML = test(region);
+
+  var num = condition_pave_cost + resurface_overlay_cost + new_construction_cost + saw_cut_patch_cost + skim_patch_cost + seal_coat_cost + stripe_cost;
+  num = num.toFixed(2);
+
+  document.getElementById("total_cost").innerHTML = "Total Cost: $" + num;
+  document.getElementById("total_spaces").innerHTML = "Total Spaces: " + parseFloat(document.getElementById("striping").value); 
+}
+
+function intialize(){
+  document.getElementById("condition_pave").setAttribute("value",0);
+  document.getElementById("resurface_overlay").setAttribute("value",0);
+  document.getElementById("new_construction").setAttribute("value",0);
+  document.getElementById("saw_cut_patch").setAttribute("value",0);
+  document.getElementById("skim_patch").setAttribute("value",0);
+  document.getElementById("seal_coat").setAttribute("value",0);
+  document.getElementById("striping").setAttribute("value",0);
+}
+
+
+
+
+/*
+    function calculate(){
+        var region = document.getElementById("region").value;
+        var condition_pave = parseFloat(document.getElementById("condition_pave").value);
+        var resurface_overlay = parseFloat(document.getElementById("resurface_overlay").value);
+        var new_construction = parseFloat(document.getElementById("new_construction").value);
+        var saw_cut_patch = parseFloat(document.getElementById("saw_cut_patch").value);
+        var skim_patch = parseFloat(document.getElementById("skim_patch").value);
+        var seal_coat = parseFloat(document.getElementById("seal_coat").value);
+        var striping = parseFloat(document.getElementById("striping").value);
+    }
+        switch (regions) {
+            case "eastern_nc":
+            // perform calculation for Eastern NC
+                switch (expression) {
+                    case value1:
+                    //Statements executed when the
+                    //result of expression matches value1
+                    break;
+                    case value2:
+                    //Statements executed when the
+                    //result of expression matches value2
+                    break;
+                    case valueN:
+                    //Statements executed when the
+                    //result of expression matches valueN
+                    break;
+                    default:
+                    //Statements executed when none of
+                    //the values match the value of the expression
+                    break;
+                }
+            break;
+            case "the_triangle":
+            // perform calculation for The Triangle
+            break;
+            case "fayetteville":
+            // perform calculation for Fayetteville
+            break;
+            case "wilmington":
+            // perform calculation for Jacksonville/Wilmington
+            break;
+            default:
+            //document.getElementById("result").innerHTML = "Please select a region.";
+            return;
+        }
+
+document.getElementById("result").innerHTML = "The estimated cost is $" + result;
+*/

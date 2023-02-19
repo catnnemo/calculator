3bff632b76841794ecbae64d6b9c309637f76771
catnnemo <njpianoforte@gmail.com>
2023-02-19 16:50:45 -0500

    Lots of edits

---
+++ learn.js
@@ -0,0 +1,12 @@
+function initialize(){
+    document.getElementById("width").setAttribute("value","0");
+    document.getElementById("height").setAttribute("value","input");
+    var width = 0;
+    var height = 0;
+  }
+  
+  function calculate() {
+      width = parseFloat(document.getElementById("width").value);
+    height = parseFloat(document.getElementById("height").value);
+    document.getElementById("output").innerHTML = width*height;
+  }
